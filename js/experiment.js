1f504e0f9a11bed1fd304f1048345ecfa4f2d943
SamsonicX <samsonicx@mail.ru>
2015-12-11 01:07:39 +0600

    Первый набросок

--- js/experiment.js
+++ js/experiment.js
@@ -13,20 +13,20 @@ var xAngle = 0, yAngle = 0;
 $('body').keydown(function(evt) {
     switch(evt.keyCode) {
         case 37: // left
-            yAngle -= 90;
+            yAngle -= 45;
             break;
 
         case 38: // up
-            xAngle += 90;
+            xAngle += 45;
             evt.preventDefault();
             break;
 
         case 39: // right
-            yAngle += 90;
+            yAngle += 45;
             break;
 
         case 40: // down
-            xAngle -= 90;
+            xAngle -= 45;
             evt.preventDefault();
             break;
     };
