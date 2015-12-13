8290454cdad403d7b2ed7a01f257f9bcbbfa0330
Shinzou <gur.ivanuil@gmail.com>
2015-12-14 00:27:53 +0600

    gia

--- js/ready.js
+++ js/ready.js
@@ -10,7 +10,10 @@ function setCookie( name, value, expires, path, domain, secure ) {
 //setCookie('number', 'qwe', new Date( (new Date()).getTime() + ( 60 * 60 * 24 * 1000 ) ), '/');
 
 $('body').find('.design').click(function() {
-    $a = document.getElementById('number').value
+	if (document.getElementById('radio1').checked) $a = document.getElementById('radio1').value;
+	if (document.getElementById('radio2').checked) $a = document.getElementById('radio2').value;
+	if (document.getElementById('radio3').checked) $a = document.getElementById('radio3').value;
+	if (document.getElementById('number').value > 1 ) $a = document.getElementById('number').value
     //setCookie('number', $a, new Date( (new Date()).getTime() + ( 60 * 60 * 24 * 1000 ) ), '/');
     var s = 'index.html?'+escape($a);
     this.href=s;
